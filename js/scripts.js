b6747a6f513edaceee8455138f30e585710be521
seenordrew <robertscalifornia@gmail.com>
2018-01-19 16:14:45 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
 				$('.site-nav').toggleClass('active');
 				$('.site-nav-btn').toggleClass('toggle');
 			})
+			$('.nav-link').click(function(){
+				$('.site-nav').toggleClass('inactive');
+				// $('.site-nav-btn').toggleClass('')
+			})
 	})
